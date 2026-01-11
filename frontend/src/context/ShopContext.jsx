1c3a610f3c05afb425faaa637eb4fe6cc0155d75
Your Name <your.email@example.com>
2026-01-11 01:42:25 -0500

    Changed URL

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -9,7 +9,7 @@ const ShopContextProvider = (props) => {
 
     const currency = '$';
     const delivery_fee = 10;
-    const backendUrl = import.meta.env.VITE_BACKEND_URL || 'https://your-vercel-backend-url.vercel.app';
+    const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:4000';
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
