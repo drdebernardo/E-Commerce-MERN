9fed14407373c400c75574873dfb2a98b078a46a
Your Name <your.email@example.com>
2026-01-26 20:46:56 -0500

    Updated

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -5,6 +5,8 @@ const connectDB = async () => {
         console.log('MongoDB connected successfully');
     })
 
+    // Debug log to check the actual URI being used
+    console.log('Connecting to MongoDB with URI:', `${process.env.MONGODB_URI}/e-commerce`);
     await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
 };
 
